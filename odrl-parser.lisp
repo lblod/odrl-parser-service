c9269f991db6ceef3f415786f3d96fc8e7337361
onodrim <alexander.van.den.berghe@redpencil.io>
2025-09-13 15:20:26 +0200

    refactor: move variables to settings file

--- odrl-parser.lisp
+++ odrl-parser.lisp
@@ -1,29 +1,5 @@
 (in-package :odrl-parser)
 
-(defparameter *backend*
-  (if (find :docker *features*)
-      ;; TODO: service name should be configurable
-      ;; TODO: bypassing auth and going directly to virtuoso to simplify development
-      "http://virtuoso:8890/sparql"
-      "http://localhost:8896")
-  "Backend triplestore to talk to.")
-
-(defparameter output-directory
-  (if (find :docker *features*)
-      "config/"
-      "examples/")
-  "The directory generated files will be written to.")
-
-(defparameter *port*
-  (if (find :docker *features*) 80 8080)
-  "The default port for the current setup.")
-
-(defparameter *repository*
-  (make-instance 'fuseki::virtuoso-repository
-                 :name "main repository"
-                 :server (make-instance 'fuseki::virtuoso-server
-                                        :base-url *backend*)))
-
 (defun boot ()
   "Function called when the service boots.
 
--- settings.lisp
+++ settings.lisp
@@ -1,5 +1,6 @@
 (in-package :odrl-parser)
 
+;; Sparql
 (defparameter sparql:*application-graph*
   (s-url (or (uiop:getenv "ODRL_POLICY_GRAPH")
              "http://mu.semte.ch/graphs/odrl-policy"))
@@ -11,3 +12,29 @@
 (defparameter sparql:*experimental-no-application-graph-for-sudo-select-queries* nil
   "when non-nil no application graph will be set when sending out sudo
   queries, thus resulting in queries across the full database.")
+
+;; Backend
+(defparameter *port*
+  (if (find :docker *features*) 80 8080)
+  "The default port for the current setup.")
+
+(defparameter *backend*
+  (if (find :docker *features*)
+      ;; TODO: service name should be configurable
+      ;; TODO: bypassing authz and going directly to virtuoso to simplify development
+      "http://virtuoso:8890/sparql"
+      "http://localhost:8896")
+  "Backend triplestore to talk to.")
+
+(defparameter *repository*
+  (make-instance 'fuseki::virtuoso-repository
+                 :name "main repository"
+                 :server (make-instance 'fuseki::virtuoso-server
+                                        :base-url *backend*)))
+
+;; TODO: Might make this configurable
+(defparameter output-directory
+  (if (find :docker *features*)
+      "config/"
+      "examples/")
+  "The directory generated files will be written to.")
