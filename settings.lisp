40f401def8feffcbd3a42de742b750d5999809fe
onodrim <alexander.van.den.berghe@redpencil.io>
2025-10-06 15:38:55 +0200

    refactor: move prefix definitions to settings file

--- settings.lisp
+++ settings.lisp
@@ -1,6 +1,23 @@
 (in-package :odrl-parser)
 
 ;; Sparql
+;; Add the used prefixes
+;; TODO(A): The necessary prefixes should be read from the policy ttl files to avoid having to update
+;; the service for policies using other prefixes. Such functionality should be added to
+;; `configuration-parssing.lisp' once the service loads ttl files instead of ntriples files.
+(add-prefix "ext" "http://mu.semte.ch/vocabularies/ext/")
+(add-prefix "odrl" "http://www.w3.org/ns/odrl/2/")
+(add-prefix "dct" "http://purl.org/dc/terms/")
+(add-prefix "vcard" "http://www.w3.org/2006/vcard/ns#")
+(add-prefix "rdf" "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+(add-prefix "mandaat" "http://data.vlaanderen.be/ns/mandaat#")
+(add-prefix "skos" "http://www.w3.org/2004/02/skos/core#")
+(add-prefix "sh" "http://www.w3.org/ns/shacl#")
+(add-prefix "lmb" "http://lblod.data.gift/vocabularies/lmb/")
+(add-prefix "besluit" "http://data.vlaanderen.be/ns/besluit#")
+(add-prefix "regorg" "http://www.w3.org/ns/regorg#")
+(add-prefix "persoon" "http://data.vlaanderen.be/ns/persoon#")
+
 (defparameter sparql:*application-graph*
   (s-url (or (uiop:getenv "ODRL_POLICY_GRAPH")
              "http://mu.semte.ch/graphs/odrl-policy"))
